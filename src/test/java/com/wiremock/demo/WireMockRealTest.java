973a8a2d1ad3a0ff2a54362767059cd41000a96a
phanduong9891 <phanduong9891@gmail.com>
2022-07-25 11:26:40 +0700

    simple wiremock crud test

--- src/test/java/com/wiremock/demo/WireMockRealTest.java
+++ src/test/java/com/wiremock/demo/WireMockRealTest.java
@@ -4,8 +4,10 @@ package com.wiremock.demo;
 import com.github.tomakehurst.wiremock.WireMockServer;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 
 import com.github.tomakehurst.wiremock.common.Json;
+import com.google.gson.Gson;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.AfterEach;
@@ -38,33 +40,43 @@ public class WireMockRealTest {
     @BeforeEach
     void wireMockServerSetUp() {
         this.restTemplate = new RestTemplate();
-        this.wireMockServer = new WireMockServer();
+        this.wireMockServer = new WireMockServer(options()
+                .dynamicPort());
         wireMockServer.start();
+        configureFor(this.wireMockServer.port());
     }
     //search room -> "number": 1, "type": "big"
 
-//
-//    @Test
-//    void testGetAll() {
-//
-//        Room room1 = new Room(1,"big");
-//        Room room2 = new Room(2,"small");
-//        List<Room> roomList = new ArrayList<>();
-//        roomList.add(room1);
-//        roomList.add(room2);
-//
-//        givenThat(get(urlEqualTo("/api/room/1"))
-//                .willReturn(aResponse()
-//                        .withBody("{\"number\": 1, \"type\": \"big\"}")));
-//
-//        String link = apiLink() + "/1";
-//
-//        //this is a request to the link
-//        ResponseEntity<String> response = restTemplate.getForEntity(link, String.class);
-//
-//        assertThat(response.getBody()).isEqualTo("{\"number\": 1, \"type\": \"big\"}");
-//
-//    }
+
+    @Test
+    void testGetAll() {
+
+        Room room1 = new Room(1,"big");
+        Room room2 = new Room(2,"small");
+        List<Room> roomList = new ArrayList<>();
+        roomList.add(room1);
+        roomList.add(room2);
+
+        Gson gson = new Gson();
+        String json = gson.toJson(roomList);
+
+        givenThat(get(urlEqualTo("/api/room/1"))
+                .willReturn(aResponse()
+                        .withBody(json)));
+
+        String link = apiLink() + "/1";
+        HttpEntity<String> getContent = new HttpEntity<>(json);
+
+        //this is a request to the link
+        ResponseEntity<String> response1 = restTemplate.exchange(
+                link,
+                HttpMethod.GET,
+                getContent,
+                String.class
+        );
+     assertThat(response1.getBody()).isEqualTo(json);
+
+    }
 
 
     @Test
@@ -111,6 +123,7 @@ public class WireMockRealTest {
                 .willReturn(aResponse()
                         .withStatus(200)));
 
+
         String link = apiLink() + "/update";
         HttpEntity<String> putContent = new HttpEntity<>(
                 "{\"number\": 2, \"type\": \"small\"}"
@@ -125,19 +138,25 @@ public class WireMockRealTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
-//  @Test
-//  void testDeleteApit(){
-//        givenThat(delete(urlEqualTo("/api/room/delete/1"))
-//                .willReturn(aResponse()
-//                        .withStatus(200)));
-//
-//        String link = apiLink() + "/1";
-//
-//        //create a delete request
-//      ResponseEntity<Void> response = restTemplate.delete(link,void.class);
-//  }
-//
+  @Test
+  void testDeleteApi(){
 
+        givenThat(delete(urlEqualTo("/api/room/delete/1"))
+                .willReturn(noContent()));
+
+        String link = apiLink() + "/delete/1";
+        HttpEntity<String> entity = new HttpEntity<>("{\"number\": 2, \"type\": \"small\"}");
+
+        //create a delete request
+      ResponseEntity<String> response = restTemplate.exchange(
+              link,
+              HttpMethod.DELETE,
+              entity,
+              String.class
+      );
+
+      assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+  }
 
     @AfterEach
     void stopWireMockServer() {
